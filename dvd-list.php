31a0b900f56cfe2f54664a2c2ac9ad20495ec6d8
ariffathur98 <37612328+ariffathur98@users.noreply.github.com>
2018-07-24 14:34:20 +0700

    Update dvd-list.php

--- dvd-list.php
+++ dvd-list.php
@@ -170,8 +170,8 @@
                       $x=1;
                       while($row = $premi->fetch_array()){
               			$id_flm = $row["ID_FILM"];
-                          $cover = $row["COVER"];
-                          $judul = $row["JUDUL_FILM"];
+                        $cover = $row["COVER"];
+                        $judul = $row["JUDUL_FILM"];
               			$tahun = $row["TAHUN_FILM"];
               			$harga = $row["HARGA_FILM"];
                           
@@ -181,7 +181,7 @@
                                   <td>'.$judul.'</td>
                                   <td>'.$tahun.'</td>
               					<td>'.$harga.'</td>
-              					<td><i class="fa fa-pencil-square-o"></i> | <a href="delete.php?id='.$id_flm.'" target="_blank"><i class="fa fa-window-close"></i></a></td>
+              					<td><a href="form_ubah.php?id='.$id_flm.'"<i class="fa fa-pencil-square-o"></i> | <a href="delete.php?id='.$id_flm.'" target="_blank"><i class="fa fa-window-close"></i></a></td>
                               </tr>';
                           
                           $x++;
